71f831c0fb3870efe10e1e9f3e8e1d3aaca18866
Luis Gabriel <luis.gabo.rex@gmail.com>
2025-08-25 17:27:43 -0600

    feat: error states

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ let $form = document.querySelector(".form");
 let $amountInput = document.getElementById("mortgageAmount");
 let $termInput = document.getElementById("mortgageTerm");
 let $rateInput = document.getElementById("interestRate");
-let $emptyResultsContainer = document.querySelector(".empty-results"); 
-let $completedResultsContainer = document.querySelector(".completed-results"); 
+let $emptyResultsContainer = document.querySelector(".empty-results");
+let $radioInputs = document.querySelectorAll('input[name="mortgageType"]');
+let $completedResultsContainer = document.querySelector(".completed-results");
+let $radioMsg = document.querySelector(".radio-msg");
 let $clearBtn = document.querySelector(".clear-btn");
+let isSomeInputEmpty = false;
 
 $form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let someSelected = Array.from($radioInputs).some(radio => radio.checked);
+    checkIsEmpty(someSelected);
+    if (!someSelected) return;
+
     let mortgageAmount = $amountInput.value;
     let mortgageterm = $termInput.value;
     let mortgageRate = $rateInput.value;
-    let mortgageType = document.querySelector('input[name="mortgageType"]:checked').value;
+    let mortgageType = "";
+    if (someSelected) mortgageType = document.querySelector('input[name="mortgageType"]:checked').value;
     //calculate
     let [amountPerMonth, total] = calculateMortgage(mortgageAmount, mortgageterm, mortgageRate, mortgageType);
     //show results
@@ -20,28 +28,26 @@ $form.addEventListener('submit', (e) => {
 })
 
 $clearBtn.addEventListener('click', (e) => {
-    $amountInput.value = '';
-    $termInput.value = '';
-    $rateInput.value = '';
+    clearAll();
     hideResults();
-    
+
 })
 
 function calculateMortgage(amount, term, rate, type) {
     let amountPerMonth = 0;
     let totalAmount = 0;
-    let ratePerMoth = (rate/100) / 12;
+    let ratePerMoth = (rate / 100) / 12;
     let totalTerms = term * 12;
     if (type == "repayment") {
-        amountPerMonth = amount * ((ratePerMoth * ( 1 + ratePerMoth)**totalTerms)/(( 1+ ratePerMoth)**totalTerms - 1));
+        amountPerMonth = amount * ((ratePerMoth * (1 + ratePerMoth) ** totalTerms) / ((1 + ratePerMoth) ** totalTerms - 1));
         totalAmount = amountPerMonth * totalTerms;
-        amountPerMonth = amountPerMonth .toFixed(2);
-        totalAmount = totalAmount .toFixed(2);
+        amountPerMonth = amountPerMonth.toFixed(2);
+        totalAmount = totalAmount.toFixed(2);
     } else {
         amountPerMonth = amount * ratePerMoth;
         totalAmount = amountPerMonth * totalTerms;
-        amountPerMonth = amountPerMonth .toFixed(2);
-        totalAmount = totalAmount .toFixed(2);
+        amountPerMonth = amountPerMonth.toFixed(2);
+        totalAmount = totalAmount.toFixed(2);
     }
 
     return [amountPerMonth, totalAmount];
@@ -60,6 +66,37 @@ function hideResults() {
 }
 
 function clearAll() {
+    $amountInput.value = '';
+    $termInput.value = '';
+    $rateInput.value = '';
+}
+
+function checkIsEmpty(someSelected) {
+    if ($amountInput.value == "") {
+        $amountInput.classList.add("empty");
+        isSomeInputEmpty = true;
+    }
+
+    if ($rateInput.value == "") {
+        $rateInput.classList.add("empty");
+        isSomeInputEmpty = true;
+    }
+    if ($termInput.value == "") {
+        $termInput.classList.add("empty");
+        isSomeInputEmpty = true;
+    }
+    if (!someSelected) {
+        $radioMsg.classList.add("show");
+        isSomeInputEmpty = true;
+    }
+
+    setTimeout(() => {
+        $amountInput.classList.remove("empty")
+        $rateInput.classList.remove("empty");
+        $termInput.classList.remove("empty");
+        $radioMsg.classList.remove("show");
+        isSomeInputEmpty = false;
+    }, "3000");
 
 }
 
